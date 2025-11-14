86ea62a5a1872c5c75b8870b6821ca18dd897ee2
JacopoRaffi <raffijacopo@gmail.com>
2025-11-14 14:00:36 +0100

    Added data paralllelism

---
+++ cpp/data_parallel/dp.cpp
@@ -0,0 +1,142 @@
+/*********************************************************************
+ *
+ * Description: C++/MPI proxy for Transformer-based models distributed training 
+ *              with data parallelism
+ * Author: Jacopo Raffi
+ *
+ *********************************************************************/
+
+#include <mpi.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../utils.hpp"
+
+// Default values
+#define NUM_B 10
+#define LOCAL_BATCH_SIZE 128
+#define WARM_UP 8
+#define RUNS 128
+
+
+/**
+ * @brief Simulates one iteration of data-parallel (using bucketing approach) training for a Transformer model.
+ *
+ * This function performs a mock forward pass and a backward pass with asynchronous
+ * all-reduce operations for gradients over multiple parameter buckets.
+ *
+ * @param grad_ptrs Array of pointers to gradient buffers for each bucket.
+ * @param sum_grad_ptrs Array of pointers to buffers storing the reduced gradients.
+ * @param num_buckets Number of parameter buckets.
+ * @param params_per_bucket Array containing the number of parameters in each bucket.
+ * @param fwd_rt_whole_model Forward pass runtime in microseconds.
+ * @param bwd_rt_per_B Backward pass runtime per bucket in microseconds.
+ * @return int Always returns 0.
+ */
+int run_data_parallel(float** grad_ptrs, float** sum_grad_ptrs, 
+                      int num_buckets, uint64_t* params_per_bucket,
+                      float fwd_rt_whole_model, float bwd_rt_per_B){
+    
+
+    printf("Data Parallelism: Starting iteration...\n");
+    //forward compute
+    usleep(fwd_rt_whole_model);
+    printf("Data Parallelism: Finished forward pass.\n");
+    //backward (idea is to overlap all-reduce with backward compute)
+    MPI_Request grad_allreduce_reqs[num_buckets];
+    //must initialize with MPI_REQUEST_NULL
+    for(int i=0; i<num_buckets; i++)
+        grad_allreduce_reqs[i] = MPI_REQUEST_NULL;
+
+    int index, flag;
+    for(int i=0; i<num_buckets; i++){
+        if(i > 1)
+            MPI_Testany(num_buckets, grad_allreduce_reqs, &index, &flag, MPI_STATUSES_IGNORE); //Checks if any of the non-blocking all-reduce requests have completed
+
+        usleep(bwd_rt_per_B); //compute backward of a bucket
+            printf("Data Parallelism: Finished backward compute for bucket %d.\n", i);
+
+        MPI_Iallreduce(grad_ptrs[i], sum_grad_ptrs[i], params_per_bucket[i], MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD, &grad_allreduce_reqs[i]);	
+    }
+
+    MPI_Waitall(num_buckets, grad_allreduce_reqs, MPI_STATUSES_IGNORE); 
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    int rank, world_size;
+
+    int num_buckets = NUM_B;
+
+    if(argc < 2){
+        std::cout << "Usage: mpirun -n <world_size> ./dp <model_name> <num_buckets>\n";
+        return -1;
+    }
+
+    std::string model_name = argv[1];
+    if(argc > 2){
+        num_buckets = std::stoi(argv[2]);
+    }
+
+    std::map<std::string, uint64_t> model_stats = get_model_stats("../../model_stats/" + model_name + ".txt"); // get model stats from file
+    
+    float fwd_rt_whole_model = model_stats["avgForwardTime"]; // in us
+    float bwd_rt_per_B = (model_stats["avgBackwardTime"]) / num_buckets; // in us
+
+    int local_batch_size = model_stats["batchSize"];
+
+    uint64_t total_model_size = model_stats["modelSize"]; // number of parameters
+    uint64_t base_params_per_bucket = total_model_size / num_buckets;
+    uint64_t remainder = total_model_size % num_buckets;
+
+    uint64_t params_per_bucket[num_buckets];
+
+    for (int i = 0; i < num_buckets; i++) {
+        params_per_bucket[i] = base_params_per_bucket + (i < remainder ? 1 : 0); // distribute remainder across the buckets
+    }
+        
+    
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    float* grad_ptrs[num_buckets];
+    float* sum_grad_ptrs[num_buckets];
+    for(int i=0; i<num_buckets; i++){
+        grad_ptrs[i] = (float *)calloc(params_per_bucket[i], sizeof(float)); //TODO: support also float16
+        sum_grad_ptrs[i] = (float *)calloc(params_per_bucket[i], sizeof(float));
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //warmup
+    for(int wmp = 0; wmp < WARM_UP; wmp++){
+        run_data_parallel(grad_ptrs, sum_grad_ptrs, num_buckets, params_per_bucket,
+                         fwd_rt_whole_model, bwd_rt_per_B);
+    }
+
+    double begin, elapse;
+    begin = MPI_Wtime();
+    for(int iter = 0; iter < RUNS; iter++){
+        run_data_parallel(grad_ptrs, sum_grad_ptrs, num_buckets, params_per_bucket,
+                         fwd_rt_whole_model, bwd_rt_per_B);
+    }
+    elapse = (MPI_Wtime()-begin)/RUNS;
+
+    if(rank == 0){
+        std::cout << "Rank = " << rank
+                  << ", world_size = " << world_size
+                  << ", data_shards = " << world_size
+                  << ", total_params = " << total_model_size
+                  << ", num_buckets = " << num_buckets
+                  << ", global_batch_size = " << (local_batch_size * world_size)
+                  << ".\n";
+        //TODO: Add here all the stats related to data parallelism execution
+    }
+
+    MPI_Finalize();
+}
--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -10,11 +10,25 @@
 #define UTILS_HPP
 
 #include <assert.h>
+#include <cstdint>
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <map>
 
+std::string extract_value(const std::string &line) {
+    size_t pos = line.find(':');
+    if (pos == std::string::npos)
+        return ""; // no delimiter → empty string
+
+    std::string value = line.substr(pos + 1);
+
+    // trim whitespace
+    value.erase(0, value.find_first_not_of(" \t\r\n"));
+    value.erase(value.find_last_not_of(" \t\r\n") + 1);
+
+    return value;
+}
 
 /**
  * @brief Reads model statistics from a stats file and returns them in a map
@@ -22,9 +36,10 @@
  * The file has this format:
  *   - Forward Flops:<value>
  *   - Backward Flops:<value>
- *   - Model Size (Bytes):<value>
+ *   - Model Size:<value>
  *   - Average Forward Time (s):<value>
  *   - Average Backward Time (s):<value>
+ *   - Batch Size:<value>
  *
  * Each parsed value is stored in the returned map with keys:
  * "forwardFlops", "backwardFlops", "modelSize", "avgForwardTime", "avgBackwardTime".
@@ -32,7 +47,7 @@
  * @param file_name Path to the model statistics file.
  * @return std::map<std::string, float>.
  */
-std::map<std::string, float> get_model_stats(std::string file_name){
+std::map<std::string, uint64_t> get_model_stats(std::string file_name){
     std::ifstream file(file_name);
 
     if (!file.is_open()) {
@@ -40,34 +55,38 @@ std::map<std::string, float> get_model_stats(std::string file_name){
         return {};
     }
 
-    std::map<std::string, float> model_stats;
+    std::map<std::string, uint64_t> model_stats;
     std::string line;
 
-    // Forward Flops
     std::getline(file, line);
-    float forwardFlops = std::stoll(line.substr(line.find(':') + 1));
+    uint64_t forwardFlops = std::stoull(extract_value(line));
 
     // Backward Flops
     std::getline(file, line);
-    float backwardFlops = std::stoll(line.substr(line.find(':') + 1));
+    uint64_t backwardFlops = std::stoull(extract_value(line));
 
     // Model Size
     std::getline(file, line);
-    float modelSize = std::stoll(line.substr(line.find(':') + 1));
+    uint64_t modelSize = std::stoull(extract_value(line));
 
-    // Average Forward Time
+    // Average Forward Time  (should be double, not uint64_t)
     std::getline(file, line);
-    float avgForwardTime = std::stod(line.substr(line.find(':') + 1));
+    uint64_t avgForwardTime = std::stod(extract_value(line));
 
-    // Average Backward Time
+    // Average Backward Time (should be double)
     std::getline(file, line);
-    float avgBackwardTime = std::stod(line.substr(line.find(':') + 1));
+    uint64_t avgBackwardTime = std::stod(extract_value(line));
+
+    // Batch size
+    std::getline(file, line);
+    uint64_t batch_size = std::stoull(extract_value(line));
 
     model_stats["forwardFlops"] = forwardFlops;
     model_stats["backwardFlops"] = backwardFlops;
     model_stats["modelSize"] = modelSize;
     model_stats["avgForwardTime"] = avgForwardTime;
     model_stats["avgBackwardTime"] = avgBackwardTime;
+    model_stats["batchSize"] = batch_size;
 
     return model_stats;   
 }
